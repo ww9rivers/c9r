42d9de5354b8cf916b30171657a4cddbd73a83ef
Wei Wang <ww@9rivers.com>
2017-01-10 16:26:40 -0500

    Has to split filter.Trim into its own file.

---
+++ util/filter/Trim.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from c9r.util.filter import Filter
+
+
+class Trim(Filter):
+    ''' Filter to trim extra spaces in (before and after) a string.
+    '''
+    def write(self, data):
+        '''Normalize given data before writing to the pipe.
+
+        /data/ is expected to be a dictionary-type object, with.
+        '''
+        return Filter.write(self, data.strip())
