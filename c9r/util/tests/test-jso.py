07b0e3b6f114e30323a9b63040462a166d008953
Wei Wang <ww@9rivers.com>
2018-12-13 17:33:35 -0500

    Made jso.test to test-jso.py for pytest

---
+++ c9r/util/tests/test-jso.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env pytest
+'''
+Module test for c9r.util.filter.csv.IO/.Reader/.Writer
+'''
+from c9r.util.jso import Storage
+
+def test_1():
+    o = Storage(a=1)
+    assert o.a == 1
+    assert o['a'] == 1
+    o.a = 2
+    assert o['a'] == 2
+    del o.a
+    assert o.a == None
+
+def test_2_getlist():
+    request = Storage()
+    request.vars = Storage()
+    request.vars.x = 'abc'
+    request.vars.y = ['abc', 'def']
+    assert request.vars.getlist('x') == ['abc']
+    assert request.vars.getlist('y') == ['abc', 'def']
+    assert request.vars.getlist('z') == []
+
+def test_3_getfirst():
+    request = Storage()
+    request.vars = Storage()
+    request.vars.x = 'abc'
+    request.vars.y = ['abc', 'def']
+    assert request.vars.getfirst('x') == 'abc'
+    assert request.vars.getfirst('y') == 'abc'
+    assert request.vars.getfirst('z') == None
+
+def test_4_getlast():
+    request = Storage()
+    request.vars = Storage()
+    request.vars.x = 'abc'
+    request.vars.y = ['abc', 'def']
+    assert request.vars.getlast('x') == 'abc'
+    assert request.vars.getlast('y') == 'def'
+    assert request.vars.getlast('z') == None
