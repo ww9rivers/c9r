5f6ea7e10925aaad536819d7734757418988e1b8
Wei Wang <ww@9rivers.com>
2019-01-02 16:53:02 -0500

    Renamed module c9r.html to c9r.text.

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(name='c9r',
       url='https://github.com/ww9rivers/c9r',
       platforms='Windows, Linux, Mac, Unix',
       packages=[
-          'c9r', 'c9r/cli', 'c9r/csv', 'c9r/file', 'c9r/html', 'c9r/mail', 'c9r/net',
-          'c9r/pim',
+          'c9r', 'c9r/cli', 'c9r/csv', 'c9r/file', 'c9r/mail', 'c9r/net',
+          'c9r/pim', 'c9r/text',
           'c9r/util', 'c9r/util/filter',
       ],
       package_data={
