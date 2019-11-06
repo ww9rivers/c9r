297d70907fa065dc447ce08b56e67d3e31547470
Wei Wang <ww@9rivers.com>
2019-11-06 16:59:37 -0500

    Revert "Revised to use Python setuptools packaging."
    This reverts commit eecdaf7ade585f5767e699317915584d7de17744.

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@
 
 from setuptools import setup
 
+<<<<<<< HEAD
 
 setup(name='c9r',
       version='0.2.3',
+=======
+setup(name='c9r',
+      version='0.1',
+>>>>>>> parent of eecdaf7... Revised to use Python setuptools packaging.
       description="""Python utility modules in the 'c9r' namespace.""",
       long_description="""
         Utility modules in the 'c9r' namespace, including IP networking, etc.
@@ -18,9 +23,10 @@ setup(name='c9r',
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License"],
+        "License :: OSI Approved :: MIT License",],
       url='https://github.com/ww9rivers/c9r',
       platforms='Windows, Linux, Mac, Unix',
+<<<<<<< HEAD
       packages=[
           'c9r', 'c9r/cli', 'c9r/csvx', 'c9r/file', 'c9r/mail', 'c9r/net',
           'c9r/pim', 'c9r/text',
@@ -33,6 +39,13 @@ setup(name='c9r',
             'c9r/util/csvfix-conf.json'
             ]
         },
+=======
+      packages=['html',
+                'mail',
+                'net',
+                'snmp'
+                ],
+>>>>>>> parent of eecdaf7... Revised to use Python setuptools packaging.
 
       install_requires=[
           'gevent',
@@ -40,6 +53,7 @@ setup(name='c9r',
           'pysnmp',
           'requests'
         ],
+<<<<<<< HEAD
       include_package_data=True,
 
       #
@@ -51,3 +65,6 @@ setup(name='c9r',
       tests_require=['pytest'],
       setup_requires=['pytest-runner'],
       zip_safe=False)
+=======
+      )
+>>>>>>> parent of eecdaf7... Revised to use Python setuptools packaging.
